50ba50a9e3e6007abba5f30bea8d25a4209b1646
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2018-01-10 07:57:08 +0100

    Fix SDL setup

--- 2048/src/main.c
+++ 2048/src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <SDL.h>
 
+const int WIDTH = 840;
+const int HEIGHT = 680;
+
 typedef struct {
 	SDL_Renderer *renderer;
 } Game;
 
-int WinMain(int argc, char **argv) {
+int main(int argc, char **argv) {
 	printf("Main!");
 
 	return 0;
